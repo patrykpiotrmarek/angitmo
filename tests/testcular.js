e02d8bab42241d46b7d22bc1349e18e73d49ec90
Patryk Marek <Patryk.Marek@skanska.pl>
2015-11-11 14:47:28 +0100

    dodanie testowego testu

---
+++ tests/testcular.js
@@ -0,0 +1,10 @@
+/* global describe */
+/* global it */
+/* global expect */
+'use strict';
+
+describe('example test', function() {
+  it('should be true', function() {
+    expect('foo').toBe('foo');
+  });
+});
